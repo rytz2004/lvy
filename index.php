926217e291074d8db6bef3bf4368da8b77b5bd66
renyuan <renyuan@inke.cn>
2019-09-30 10:42:26 +0800

    页面调整

--- index.php
+++ index.php
@@ -2,8 +2,11 @@
 
 header("Content-type: text/html; charset=UTF-8");
 
-$name= $_POST["id"];
-
-
-echo "你输入的名字为：".$name;
+$array = array();
+$id= $_POST["id"];
+$array['id']=$id;
+$array['code'] = 200;
+$array['msg'] = "成功";
+echo json_encode($array,true);
+//echo "你输入的名字为：".$name;
 ?>
