8fd4c5f4590b63df300595b9574b369e229389e5
renyuan <renyuan@inke.cn>
2019-09-30 11:25:49 +0800

    chang code

--- index.php
+++ index.php
@@ -4,9 +4,17 @@ header("Content-type: text/html; charset=UTF-8");
 
 $array = array();
 $id= $_POST["id"];
-$array['id']=$id;
-$array['code'] = 200;
-$array['msg'] = "成功";
+if(isset($id)){
+    $array['id']=$id;
+    $array['code'] = -1;
+    $array['msg'] = "成功";
+    echo json_encode($array,true);
+}
+
+$array['id']=0;
+$array['code'] = -1;
+$array['msg'] = "失败";
 echo json_encode($array,true);
+
 //echo "你输入的名字为：".$name;
 ?>
