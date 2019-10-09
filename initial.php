1f97d31e6e25196e7b5702758c880cbffff8c33c
rytz2004 <332105790@qq.com>
2019-10-10 01:00:01 +0800

    抽奖修改

--- initial.php
+++ initial.php
@@ -10,12 +10,44 @@ mysqli_query($connect,$sql);
 // 选择数据表
 
 //查询单条数据并以json的格式输出
-$sql = "select * from lv_award where id desc";
-// 执行sql语句返回结果集
-$result = mysqli_query($connect,$sql);
-$row = @mysqli_fetch_row($result);
-print_r($row);die;
-echo  json_encode($row);
+$sql_2 = "select user_name,user_phone from lv_award where type=2";
+$result_2 = mysqli_query($connect,$sql_2);
+$row_2 = @mysqli_fetch_all($result_2);
 
+$sql_3 = "select user_name,user_phone from lv_award where type=3";
+$result_3 = mysqli_query($connect,$sql_3);
+$row_3 = @mysqli_fetch_all($result_3);
+
+$sql_4 = "select user_name,user_phone from lv_award where type=4";
+$result_4 = mysqli_query($connect,$sql_4);
+$row_4 = @mysqli_fetch_all($result_4);
+
+$sql_5 = "select user_name,user_phone from lv_award where type=5";
+$result_5 = mysqli_query($connect,$sql_5);
+$row_5 = @mysqli_fetch_all($result_5);
+
+$sql_6 = "select user_name,user_phone from lv_award where type=6";
+$result_6 = mysqli_query($connect,$sql_6);
+$row_6 = @mysqli_fetch_all($result_6);
+
+$sql_7 = "select user_name,user_phone from lv_award where type=7";
+$result_7 = mysqli_query($connect,$sql_7);
+$row_7 = @mysqli_fetch_all($result_7);
+
+$sql_8 = "select user_name,user_phone from lv_award where type=8";
+$result_8 = mysqli_query($connect,$sql_8);
+$row_8 = @mysqli_fetch_all($result_8);
+
+$array['info_2']=$row_2;
+$array['info_3']=$row_3;
+$array['info_4']=$row_4;
+$array['info_5']=$row_5;
+$array['info_6']=$row_6;
+$array['info_7']=$row_7;
+$array['info_8']=$row_8;
+$array['code'] = 200;
+$array['msg'] = "成功";
+
+echo json_encode($array,true);return;
 
 ?>
--- ins.php
+++ ins.php
@@ -4,17 +4,44 @@ header("Content-type: text/html; charset=UTF-8");
 
 $array = array();
 $id= $_POST["good"];
+$user= $_POST["user"];
+$phone= $_POST["phone"];
+
 $id = intval($id);
-if(isset($id)){
+if(isset($id) && isset($user) && isset($phone)){
 
     $connect = mysqli_connect('120.25.79.227','renyuan','string123','lv') or die('Unale to connect');
     if ($connect->connect_error) {
         die("连接失败: " . $connect->connect_error);
     }
-    $array['id']=0;
-    $array['code'] = -1;
-    $array['msg'] = "失败";
-    echo json_encode($array,true);return;
+
+    $sql_where = "select * from lv_award where user_phone = '$phone'";
+    $result_where = mysqli_query($connect,$sql_where);
+    $row_where = @mysqli_fetch_row($result_where);
+
+    if(!empty($row_where)){
+        $array['id']=$row_where;
+        $array['code'] = -1;
+        $array['msg'] = "手机号重复";
+        echo json_encode($array,true);return;
+    }
+
+    $create_time = date("Y-m-d H:i:s",time());
+    $sql = "INSERT INTO lv_award (user_name,user_phone,type,status,create_time) VALUES ('$user','$phone',$id,1,'$create_time')";
+    $result = mysqli_query($connect,$sql);
+    $row = @mysqli_fetch_row($result);
+    if($result>0){
+        $array['id']=$row;
+        $array['code'] = 200;
+        $array['msg'] = "成功";
+        echo json_encode($array,true);return;
+    }else{
+        $array['id']=$row;
+        $array['code'] = -1;
+        $array['msg'] = "失败";
+        echo json_encode($array,true);return;
+    }
+
 }
 
 $array['id']=0;
